4b5f8763a9087de33995b99017948230877a043d
HerbalGreenTea <alex.andros.solovev@gmail.com>
2023-03-19 19:26:25 +0500

    init project

---
+++ backend_automated_plant_care/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "automated_plant_care.backend_automated_plant_care"
---
+++ backend_automated_plant_care/src/main/kotlin/automated_plant_care/plugins/Routing.kt
@@ -0,0 +1,13 @@
+package automated_plant_care.plugins
+
+import io.ktor.server.routing.*
+import io.ktor.server.response.*
+import io.ktor.server.application.*
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+}
